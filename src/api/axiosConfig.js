7556b821c004ea0368cf495b89095e6fbc94b65e
Shraman Jana <shraman2000@gmail.com>
2024-06-11 20:58:33 +0530

    Updated backend public dns url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://ec2-13-51-121-61.eu-north-1.compute.amazonaws.com:8080/",
+    baseURL: "http://ec2-16-171-40-45.eu-north-1.compute.amazonaws.com:8080/",
     // headers: {"Access-Control-Allow-Origin": "http://localhost:3000"}
 });
